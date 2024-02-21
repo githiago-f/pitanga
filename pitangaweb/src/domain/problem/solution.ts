9891d514fbd1852aba977bdc14411da3be2425fc
Thiago <52945005+githiago-f@users.noreply.github.com>
2024-02-21 10:40:36 +0000

    creating solution on client

--- pitangaweb/src/domain/problem/solution.ts
+++ pitangaweb/src/domain/problem/solution.ts
@@ -1,3 +1,18 @@
+import { Challenge } from './challenge';
+
+export enum SolutionStatus {
+  OK = 'Ok'
+}
+
 export class Solution {
+  public readonly id: string;
+  public readonly version: number;
+  public readonly code: string;
+  public readonly language: string;
+
+  public readonly challenge: Challenge;
+
+  public get status(): SolutionStatus {
     
+  }
 }
