0e4193a5baf2c201e22d14a9da691392c15cece0
Thiago <52945005+githiago-f@users.noreply.github.com>
2024-03-18 12:19:43 +0000

    add editor controls

--- pitangaweb/src/app/pages/ChallengeEditor.tsx
+++ pitangaweb/src/app/pages/ChallengeEditor.tsx
@@ -1,11 +1,21 @@
 import { plainToInstance } from 'class-transformer';
 
-import { Challenge } from '../domain/problem';
+import { Challenge } from '../../domain/problem';
 
 import { ValidationContainer } from '../components/validation';
 import { Editor, EditorConfigContext, defaultEditorConfig } from '../components/editor';
 
-const challenge = plainToInstance(Challenge, {});
+const challenge = plainToInstance(Challenge, {
+  id: 'uuid-challenge',
+  title: 'Find the summation',
+  description: '',
+  validations: [
+    { input: '1 2', output: '3', resultStatus: 'void' },
+    { input: '3 2', output: '5', resultStatus: 'void' },
+    { input: '2 3', output: '5', resultStatus: 'void' },
+    { input: '1 1', output: '2', resultStatus: 'void' }
+  ]
+});
 
 export const ChallengeEditor = () => (
   <>
--- pitangaweb/src/components/validation/test-item.tsx
+++
@@ -1,5 +0,0 @@
-import { Validation } from '../../domain/problem';
-
-export const ValidationItem = (props: Validation) => (
-  <li><b>{props.input}</b> {props.resultStatus}</li>
-);
