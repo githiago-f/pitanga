26f109f19869c2c44b14f6ea0fb1c5d83f8e09f9
githiago-f <tfarias@protonmail.com>
2024-04-14 16:35:50 -0300

    add configuration variables

--- pitangaweb/src/App.tsx
+++ pitangaweb/src/App.tsx
@@ -1,27 +1,29 @@
-import { StrictMode } from "react";
-import { Component } from "react";
-import { ChallengesList } from "./app/pages/ChallengesList";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { ChallengeEditor } from "./app/pages/ChallengeEditor";
-import { getChallenge, listChallenges } from "./infra/data/pitanga.rest";
-import { ErrorPage } from "./app/pages/ErrorPage";
+import { StrictMode } from 'react';
+import { Component } from 'react';
+import { ChallengesList } from './app/pages/ChallengesList';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ChallengeEditor } from './app/pages/ChallengeEditor';
+import { getChallenge, listChallenges } from './infra/data/pitanga.rest';
+import { ErrorPage } from './app/pages/ErrorPage';
+
+const basename = import.meta.env.BASE_URL ?? '/pitanga-tcc';
 
 // using this component style to provide error handling context
 export class App extends Component {
   router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <ChallengesList />,
       loader: listChallenges,
       errorElement: <ErrorPage />,
     },
     {
-      path: "/:challengeId",
+      path: '/:challengeId',
       element: <ChallengeEditor />,
       loader: getChallenge,
       errorElement: <ErrorPage />,
     }
-  ])
+  ], { basename });
 
   render() {
     return (
