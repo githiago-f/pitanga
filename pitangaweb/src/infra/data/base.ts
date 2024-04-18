46529694c003228b1f39b34c1ebd8f30bdf805b0
githiago-f <tfarias@protonmail.com>
2024-04-17 22:05:59 -0300

    make axios send credentials on request

--- pitangaweb/src/infra/data/base.ts
+++ pitangaweb/src/infra/data/base.ts
@@ -4,5 +4,6 @@ import { restConfig } from '../../app/config/rest.config';
 export const apiBase = axios.create({
   baseURL: restConfig.baseURL,
   timeout: 30000,
-  validateStatus: (status) => [200, 404].includes(status)
+  validateStatus: (status) => [200, 404].includes(status),
+  withCredentials: true
 });
