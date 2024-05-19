4f15974c1d2555fcee5bbdc9e149c686bc3266a6
githiago-f <tfarias@protonmail.com>
2024-05-19 15:41:12 -0300

    fix text wrap on title

--- pitangaweb/src/app/components/tool-tray/Title.tsx
+++ pitangaweb/src/app/components/tool-tray/Title.tsx
@@ -1,5 +1,5 @@
 export const Title = (props: { title: string; }) => (
-  <h2 className='text-xl font-bold text-ellipsis overflow-hidden'>
+  <h2 className='text-xl max-h-6 font-bold text-ellipsis overflow-hidden text-nowrap'>
     {props.title}
   </h2>
 );
