af7b2c775c8506ee54822fc6ebcc5c6dec81777d
githiago-f <tfarias@protonmail.com>
2025-05-31 17:34:36 -0300

    remove legacy pitanga-code

--- core/src/main/java/br/edu/ifrs/pitanga/core/infra/runners/CommandRunner.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifrs.pitanga.core.infra.runners;
-
-import br.edu.ifrs.pitanga.core.infra.runners.vo.Submission;
-import reactor.core.publisher.Mono;
-
-public interface CommandRunner {
-    Mono<String> execute(Submission submissionDTO);
-}
--- core/src/main/java/br/edu/ifrs/pitanga/core/infra/runners/vo/CompileStatus.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifrs.pitanga.core.infra.runners.vo;
-
-public enum CompileStatus {
-    FAILURE, SUCCESS
-}
