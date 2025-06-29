53a6cdadf45b2b9a6dd5c668b710080bf1921ef5
githiago-f <tfarias@protonmail.com>
2025-06-29 17:52:32 -0300

    fix init command

--- install.sh
+++ install.sh
@@ -8,13 +8,17 @@ fi
 KEY_PATH="$CERT_PATH/key.pem"
 CERT_PATH="$CERT_PATH/certificate.pem"
 
+if (!(test -d ./core/src/main/resources/certs)); then
+    mkdir ./core/src/main/resources/certs
+fi
+
 if (!(test -e $CERT_PATH)); then
     openssl req -x509 -newkey rsa:4096 -nodes -keyout $KEY_PATH -out $CERT_PATH \
         -sha256 -days 3650 -nodes \
         -subj "/C=BR/ST=RioGrandeDoSul/L=PortoAlegre/O=IFRSPitanga/OU=TI/CN=$CommonName"
 
-    keytool -delete -cacerts -alias  pitanga -storepass $STORE_PASS
-    keytool -importcert -cacerts -file $CERT_PATH -alias pitanga -storepass $STORE_PASS
+    keytool -delete -cacerts -alias  pitanga #-storepass $STORE_PASS
+    keytool -importcert -cacerts -file $CERT_PATH -alias pitanga # -storepass $STORE_PASS
 
     sudo chmod og+r $KEY_PATH
     docker compose up -d --build keycloak
@@ -23,11 +27,11 @@ fi
 docker compose up -d postgres keycloak
 
 export SHOW_SQL=false
-export DB_URL="jdbc:postgresql://<database_host>:<db_port>/<db_name>"
-export DB_PASSWORD="<db_password>"
-export DB_USERNAME="<db_user>"
-export ISSUER_URI="https://<keycloak_host>/realms/master"
-export JWK_SET_URI="https://<keycloak_host>/realms/master/protocol/openid-connect/certs"
+export DB_URL="jdbc:postgresql://localhost:5432/challenges_pitanga"
+export DB_PASSWORD="mysecretpassword"
+export DB_USERNAME="postgres"
+export ISSUER_URI="https://localhost:8444/realms/master"
+export JWK_SET_URI="https://localhost:8444/realms/master/protocol/openid-connect/certs"
 
 cd ./core
 ./mvnw clean package
