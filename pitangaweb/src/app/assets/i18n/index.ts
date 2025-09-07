f14cd3b21b42d1fd0fc24f55e286ec221f95a977
githiago-f <52945005+githiago-f@users.noreply.github.com>
2025-09-06 22:33:15 -0300

    deleting legacy pitangaweb

--- pitangaweb/src/app/assets/i18n/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Temporary. This file should be replaced with
- * a better translation feature.
- */
-
-export {default as Labels} from './pt-br_labels.json';
-
--- pitangaweb/src/app/config/rest.config.ts
+++
@@ -1,5 +0,0 @@
-const apiBase = import.meta.env.PITANGA_API_BASE_URL;
-
-export const restConfig = {
-  baseURL: apiBase && apiBase.trim() !== '' ? apiBase : 'https://localhost:8443'
-};
